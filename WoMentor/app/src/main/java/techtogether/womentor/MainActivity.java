2fbde64d5f72d3a240c294928f1b6b312d5a67a4
Soha Jiwani <sohajiwani1@gmail.com>
2019-03-23 21:58:54 -0400

    the resources menu added

--- WoMentor/app/src/main/java/techtogether/womentor/MainActivity.java
+++ WoMentor/app/src/main/java/techtogether/womentor/MainActivity.java
@@ -2,6 +2,12 @@ package techtogether.womentor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExpandableListView expandableListView = findViewById(R.id.expandListView);
+
+        HashMap<String, List<String>> item = new HashMap<>();
+
+        ArrayList<String> imposterSyndromResources = new ArrayList<>();
+        imposterSyndromResources.add("Ways of Overcoming Imposter Syndrome from ADA Initiative: \n https://adainitiative.org/continue-our-work/impostor-syndrome-training/");
+        imposterSyndromResources.add("Professional Insights on Imposter Syndrome: \n https://hbr.org/2008/05/overcoming-imposter-syndrome");
+
+        item.put("Imposter Syndrome", imposterSyndromResources);
+
+        ArrayList<String> anxietyResources = new ArrayList<>();
+        anxietyResources.add("Anxiety and Depression Association of America: \n https://adaa.org/");
+        anxietyResources.add("Information and Possible Treatments for Anxiety: \n https://www.rtor.org/anxiety/");
+        anxietyResources.add("More resources: \n https://nationalsocialanxietycenter.com/resources/");
+
+        item.put("Anxiety", anxietyResources);
+
+        ArrayList<String> depressionResources = new ArrayList<>();
+
+        depressionResources.add("National Alliance on Mental Illness (NAMI): \n https://www.nami.org/");
+        depressionResources.add("To Write Love On Her Arms (TWLOHA): \n https://twloha.com/");
+        depressionResources.add("Mental Health First Aid: \n https://www.mentalhealthfirstaid.org/");
+
+        item.put("Depression", depressionResources);
+
+        MyExpandableListAdapter adapter = new MyExpandableListAdapter(item);
+        expandableListView.setAdapter(adapter);
     }
 }
